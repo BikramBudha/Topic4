91d78242c879565d523856820cd18fe8d658bb5b
BikramBudha <bikrambudha95@gmail.com>
2019-10-25 13:49:41 +0545

    update

--- app/src/main/java/com/bikram/topic4/MainActivity.java
+++ app/src/main/java/com/bikram/topic4/MainActivity.java
@@ -3,12 +3,61 @@ package com.bikram.topic4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView lv ;
+    public static final String countries[] = {
+            "Nepal", "Kathmandu",
+            "India", "Delhi",
+            "China", "Beijing",
+            "UK",    "London",
+            "US",    "Washingtone, DC",
+            "Canada", "Ottawa"
+    };
+    private Map<String, String> countriesMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv=findViewById(R.id.lv);
+        countriesMap=new HashMap<>();
+        for (int i = 0; i < countries.length; i +=2){
+
+            countriesMap.put(countries[i], countries[i+1]);
+        }
+        ArrayAdapter countryAdapter=new ArrayAdapter(
+                 this,
+        android.R.layout.simple_list_item_1,new ArrayList<String>(countriesMap.keySet())
+        );
+        lv.setAdapter(countryAdapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String country=parent.getItemAtPosition(position).toString();
+                String capital=countriesMap.get(country);
+
+                Toast.makeText(MainActivity.this, "Capital of"+country+"is"+capital, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    ArrayAdapter myadapter=new ArrayAdapter<>(
+            this,
+            android.R.layout.simple_list_item_1,
+            countries
+    );
+
+    lv.setAdapter(myadapter);
     }
+
 }
---
+++ app/src/main/java/com/bikram/topic4/Topic4Activity.java
@@ -0,0 +1,14 @@
+package com.bikram.topic4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Topic4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topic4);
+    }
+}
